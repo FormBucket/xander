d2e00af10349ad8de04204d916a9e3f6cfd508af
JC <jcfisher007@gmail.com>
2017-11-24 09:29:33 -0800

    v0.3.2

--- src/windowStore.js
+++ src/windowStore.js
@@ -1,12 +1,13 @@
 import {createStore, dispatch} from 'fluxury';
 
+let getSize = () => ({ width: window.innerWidth, height: window.innerHeight })
 let windowStore = createStore('window', {
   getInitialState: () => ({ width: window.innerWidth, height: window.innerHeight }),
-  windowResized: (state, action) => ({ width: window.innerWidth, height: window.innerHeight })
+  windowResized: (state, data) => data
 });
 
 function resize() {
-  dispatch('windowResized')
+  dispatch('windowResized', getSize())
 }
 
 window.onresize = resize;
